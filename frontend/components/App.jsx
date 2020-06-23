e19795c195c06e39dc6b79b0ec1dd09ce44dfa7a
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-22 21:36:53 -0700

    redux cycle set up for user auth

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>beyondPixels</h1>
+    </div>
+);
+
+export default App;
---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user);
+    }
+}
+
+export default SessionForm;
