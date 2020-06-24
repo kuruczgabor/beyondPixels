b2beeea6dfc07c0db0e87cdadca4730a23bab3d5
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-24 16:05:15 -0700

    finalizing sign up styling

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -34,37 +34,66 @@ class SessionForm extends React.Component {
         );
     }
 
+    sessionFormTopChooser() {
+        if (this.props.formType === "Sign up") {
+            return (
+                <p className="session-form-top">Join beyondPixels</p>
+            ) 
+        } else {
+            return (
+                <p className="session-form-top">Log in to beyondPixels</p>
+            )
+        }
+    }
+
+    sessionFormBottomChooser() {
+        if (this.props.formType === "Sign up") {
+            return (
+                <p className="session-form-bottom">Already have an account?  
+                    <div className="session-form-bottom-link">{this.props.navLink}</div>
+                </p>
+            ) 
+        } else {
+            return (
+                <p className="session-form-bottom">Don't have an account?  
+                    <div className="session-form-bottom-link">{this.props.navLink}</div>
+                </p>
+            )
+        }
+    }
+
     render() {
         return (
             <div className="session-form-container">
-                <form onSubmit={this.handleSubmit}>
 
-                {this.renderErrors()}
-                
-                    <div>
-                        <label>Username:
-                            <input type="text"
-                                value={this.state.username}
-                                onChange={this.update('username')}
-                            />
-                        </label>
+                {this.sessionFormTopChooser()}
 
-                        <label>Password:
-                            <input type="password"
-                                value={this.state.password}
-                                onChange={this.update('password')}
-                            />
-                        </label>
-                    
-                        <input type="submit" value={this.props.formType} />
+                <form onSubmit={this.handleSubmit} className="session-form">
+
+                    {this.renderErrors()}
+
+                    <label>Username
+                        <input type="text"
+                            value={this.state.username}
+                            onChange={this.update('username')}
+                        />
+                    </label>
+
+                    <label>Password
+                        <input type="password"
+                            value={this.state.password}
+                            onChange={this.update('password')}
+                        />
+                    </label>
                             
-                    </div>
+
+
+                    <input type="submit" value={this.props.formType} />
 
                 </form>
-
-                <p>
-                    { this.props.navLink }
-                </p>
+                
+                {this.sessionFormBottomChooser()}
+                
 
             </div>
         );
@@ -73,3 +102,8 @@ class SessionForm extends React.Component {
 }
 
 export default SessionForm;
+
+{/* <input type="submit" value={this.props.formType} /> */}
+{/* <button value={this.props.formType}>Sign up</button> */ }
+// { this.props.navLink }
+
