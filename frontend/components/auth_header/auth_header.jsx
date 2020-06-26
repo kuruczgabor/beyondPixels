c83d0ea4347e2735a5ba2012e3ccf8f1dacf1351
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-25 21:11:13 -0700

    dropdown works; need to fix the off-click

--- frontend/components/auth_header/auth_header.jsx
+++ frontend/components/auth_header/auth_header.jsx
@@ -15,33 +15,28 @@ const AuthHeader = ({ currentUser, logout }) => {
     );
 
     const signedInHeader = () => (
-
-        <nav className="signed-in-header">
-            <ul>
-
-
-
-
-                <Link to="/"><li id="profile-pointer" className="fas fa-user" onClick={revealDropdown}></li>
-                    <ul id="profile-dropdown" >
-                        <li className="profile-dropdown-item">Hello</li>
-                        <button onClick={logout}><li className="profile-dropdown-item">Log Out</li></button>
-                    </ul>
-                </Link>
-
-
-
-
-                <Link to="/" id="header-upload-button"><li><i className="fas fa-arrow-up"></i> Upload</li></Link>
+        <div id="profilepointer" onClick={triggerDropdown}>
+            <div >
+                <i className="fas fa-user-alt fa-lg"></i>
+            </div>
+            <ul id="profiledropdown">
+                <li><Link to={`/api/users/user.id`} className="profileLink">Profile</Link></li>
+                <li><Link to={`/api/users/`} className="profileLink">Galleries</Link></li>
+                <li><Link to={`/api/users/`} className="profileLink">Liked photos</Link></li>
+                <br />
+                <li onClick={logout}>Logout</li>
             </ul>
-        </nav>
-    );
+        </div>
+    )
 
-    const revealDropdown = () => {
-        $('#profile-dropdown').toggleClass('reveal')
+    const triggerDropdown = () => {
+        $('#profiledropdown').toggleClass('trigger')
     }
 
+
+
     return currentUser ? signedInHeader() : sessionLinks();
+
 };
 
 export default AuthHeader;
@@ -85,3 +80,79 @@ export default AuthHeader;
     //     <button onClick={logout}><li>Log Out</li></button>
     // </ul>
     // <Link to="/" id="header-upload-button"><li><i className="fas fa-arrow-up"></i> Upload</li></Link> */}
+
+        // const revealDropdown = () => {
+    //     $('#profile-dropdown').addClass('reveal');
+    //     $(document.body).removeClass('reveal')
+    // }
+
+    // const removeDropdown = () => {
+
+    // }
+
+
+    // ==================
+
+
+
+{/* <nav className="signed-in-header">
+    <ul>
+
+        <Link to="/"><li id="profile-dropdown-trigger" className="fas fa-user" ></li>
+
+            <ul id="profile-dropdown" className="profile-dropdown hidden">
+                <li>Profile</li>
+                <button onClick={logout}><li>Log Out</li></button>
+            </ul>
+        </Link>
+
+
+
+        <Link to="/" id="header-upload-button"><li><i className="fas fa-arrow-up"></i> Upload</li></Link>
+    </ul>
+</nav> */}
+
+
+// ===================
+
+// const signedInHeader = () => (
+
+//     <nav className="signed-in-header">
+
+//         <Link id="profile-dropdown-trigger" to="/" className="fas fa-user">
+
+//             <ul id="profile-dropdown" className="profile-dropdown">
+//                 <li>Hello</li>
+//             </ul>
+
+//         </Link>
+
+//         <Link to="/"> Upload</Link>
+
+//     </nav>
+
+// );
+
+// const triggerDropdown = () => {
+//     $('#profile-dropdown').toggleClass('reveal')
+// }
+
+// // const revealDropdown = () => {
+// //     $(document.body)
+// //     $('#profile-dropdown').addClass('hidden');
+// //     // $('#profile-dropdown-trigger').off('click', revealDropdown);
+// //     $(document).on('click', hideDropdown);
+// // };
+
+// // const hideDropdown = () => {
+// //     $('#profile-dropdown').removeClass('hidden');
+// //     // $('#profile-dropdown-trigger').on('click', revealDropdown);
+// //     $(document).off('click', hideDropdown);
+// // };
+
+// // $(() => $('#profile-dropdown-trigger').on('click', revealDropdown));
+
+// return currentUser ? signedInHeader() : sessionLinks();
+// };
+
+// export default AuthHeader;
