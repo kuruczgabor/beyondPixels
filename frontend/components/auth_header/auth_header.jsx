afbc0dffc2e6d7770374c63fce0dd55a0d6445bd
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-25 18:44:06 -0700

    dropdown works

--- frontend/components/auth_header/auth_header.jsx
+++ frontend/components/auth_header/auth_header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import ProfileDropdown from './profile_dropdown'
 
 const AuthHeader = ({ currentUser, logout }) => {
+
     const sessionLinks = () => (
         <nav className="auth-header">
             <ul>   
@@ -11,17 +13,33 @@ const AuthHeader = ({ currentUser, logout }) => {
             </ul>
         </nav>
     );
+
     const signedInHeader = () => (
+
         <nav className="signed-in-header">
-                <ul>
-                    <Link to="/"><li><i className="fas fa-user"></i></li></Link>
-                    {/* <li>Hello, {currentUser.username}!</li> */}
-                    {/* <div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
-                    <button onClick={logout}><li>Log Out</li></button>
-                    <Link to="/" id="header-upload-button"><li><i class="fas fa-arrow-up"></i> Upload</li></Link>
-                </ul>
+            <ul>
+
+
+
+
+                <Link to="/"><li id="profile-pointer" className="fas fa-user" onClick={revealDropdown}></li>
+                    <ul id="profile-dropdown" >
+                        <li className="profile-dropdown-item">Hello</li>
+                        <button onClick={logout}><li className="profile-dropdown-item">Log Out</li></button>
+                    </ul>
+                </Link>
+
+
+
+
+                <Link to="/" id="header-upload-button"><li><i className="fas fa-arrow-up"></i> Upload</li></Link>
+            </ul>
         </nav>
     );
+
+    const revealDropdown = () => {
+        $('#profile-dropdown').toggleClass('reveal')
+    }
 
     return currentUser ? signedInHeader() : sessionLinks();
 };
@@ -29,3 +47,41 @@ const AuthHeader = ({ currentUser, logout }) => {
 export default AuthHeader;
 
 
+// const AuthHeader = ({ currentUser, logout }) => {
+
+//     const sessionLinks = () => (
+//         <nav className="auth-header">
+//             <ul>
+//                 <Link to="/"><li>Demo</li></Link>
+//                 <Link to="/login"><li>Log in</li></Link>
+//                 <Link to="/signup" id="header-signup-button"><li>Sign up</li></Link>
+//             </ul>
+//         </nav>
+//     );
+
+//     const signedInHeader = () => (
+//         <nav className="signed-in-header">
+//             {/* <ProfileDropdown/> */}
+//             <ul>
+//                 <Link to="/"><li id="profile-dropdown" className="fas fa-user profile-dropdown hidden"></li></Link>
+//                 {/* <li>Hello, {currentUser.username}!</li> */}
+//                 {/* <div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+//                 <button onClick={logout}><li>Log Out</li></button>
+//                 <Link to="/" id="header-upload-button"><li><i className="fas fa-arrow-up"></i> Upload</li></Link>
+//             </ul>
+//         </nav>
+//     );
+
+//     return currentUser ? signedInHeader() : sessionLinks();
+// };
+
+{/* <li className="fas fa-user"></li>
+    <div className="fas fa-user" onClick={revealDropdown}>
+        <i ></i>
+    </div>
+    <ul id="profile-dropdown">
+        {/* <li>Hello, {currentUser.username}!</li> */}
+        {/* <div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+    //     <button onClick={logout}><li>Log Out</li></button>
+    // </ul>
+    // <Link to="/" id="header-upload-button"><li><i className="fas fa-arrow-up"></i> Upload</li></Link> */}
---
+++ frontend/components/auth_header/profile_dropdown.jsx
@@ -0,0 +1,49 @@
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+
+
+// const ProfileDropdown = ({ logout }) => {
+//     const triggerDropdown = () => {
+//         $('#profiledropdown').toggleClass('trigger')
+//     }
+
+//     return (
+//         <div id="profilepointer" onClick={triggerDropdown}>
+//             <div >
+//                 <i className="fas fa-user" ></i>
+//             </div>
+//             <ul id="profiledropdown">
+//                 <button onClick={logout}><li>Log Out</li></button>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// export default ProfileDropdown
+
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+
+
+const ProfileDropdown = ({ logout }) => {
+    const triggerDropdown = () => {
+        $('#profiledropdown').toggleClass('trigger')
+    }
+
+    return (
+        <div id="profilepointer" onClick={triggerDropdown}>
+            <div >
+                <i className="fas fa-user-alt fa-lg"></i>
+            </div>
+            <ul id="profiledropdown">
+                <li><Link to={`/api/users/user.id`} className="profileLink">Profile</Link></li>
+                <li><Link to={`/api/users/`} className="profileLink">Galleries</Link></li>
+                <li><Link to={`/api/users/`} className="profileLink">Liked photos</Link></li>
+                <br />
+                <li onClick={logout}>Logout</li>
+            </ul>
+        </div>
+    )
+}
+
+// export default ProfileDropdown
