ab0ed31ae01b1319a28bbb0e016f4347ef4fb152
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-26 15:38:39 -0700

    commiting before break

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -61,7 +61,7 @@ class SessionForm extends React.Component {
     }
 
     errorPopUp() {
-        if (this.props.errors.length > 0) {
+        if (this.props.formType === "Log in" && this.props.errors.length > 0) {
             return (
                 <div className='error-popup'>
                     {this.renderErrors()}
