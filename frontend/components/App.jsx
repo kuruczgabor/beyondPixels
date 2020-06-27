c51300e1fb9913b49bd8d0b372698c38f2126558
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-26 19:00:39 -0700

    start working on splash

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,7 +4,7 @@ import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import GreetingContainer from './auth_header/auth_header_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Modal from './modal/modal'
 
 const App = () => (
