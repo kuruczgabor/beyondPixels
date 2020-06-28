17d9dda0b5b5129a054e59129a184e482c1bd5d1
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-06-27 21:26:39 -0700

    splash page updated

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -13,7 +13,32 @@ class Splash extends Component {
                         <br/>
                         <Link to="/signup" id="splash-signup-button">Sign up</Link>
                     </div>
-                    
+                    <p id="splash-background-author">Photo by Gabor Kurucz</p>
+                </div>
+
+                <div className="splash-content">
+                    <p className="splash-content-title">What makes us different</p>
+                    <ul className="splash-content-list">
+                        <li>
+                            <p className="splash-icon" id="splash-icon-1"></p>
+                            <br/>
+                            Grow as a photographer
+                            <p>Share your photos to the community, and receive invaluable and insightful feedback.</p>
+                        </li>
+                        <li>
+                            <p className="splash-icon" id="splash-icon-2"></p>
+                            <br/>
+                            Build your career
+                            <p>Display your services, showcase workshops, and create galleries to highlight your work.</p>
+                        </li>
+                        <li>
+                            <p className="splash-icon" id="splash-icon-3"></p>
+                            <br/>
+                            Make connections
+                            <p>Meet friends, mentors, collaborative partners, and potential clients.</p>
+                        </li>
+
+                    </ul>
                 </div>
 
             </div>
@@ -23,3 +48,4 @@ class Splash extends Component {
 }
 
 export default Splash;
+
