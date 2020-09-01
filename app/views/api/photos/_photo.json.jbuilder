5e88cd6729d2a5fc0190d9cca63fa2428388b226
kuruczgabor <gabor.kurucz.us@gmail.com>
2020-09-01 12:36:36 -0700

    Add date to photoShow

--- app/views/api/photos/_photo.json.jbuilder
+++ app/views/api/photos/_photo.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! photo, :id, :title, :description, :author_id
+json.extract! photo, :id, :title, :description, :author_id, :created_at, :updated_at
 json.photoUrl url_for(photo.photo_file)
--- frontend/components/photo_show/photo_show.jsx
+++ frontend/components/photo_show/photo_show.jsx
@@ -20,11 +20,12 @@ class PhotoShow extends React.Component {
 
     render() {
         if (!this.props.photo) return null;
-        const { title, description, photoUrl } = this.props.photo;
+        const { title, description, photoUrl, created_at } = this.props.photo;
 
+        const createdAt = this.props.photo.created_at.slice(0, 10).split('-')
+        const formattedCreatedAt = createdAt[1] + '/' + createdAt[2] + '/' + createdAt[0]
 
         const photoAuthor = this.props.photoAuthor ? this.props.photoAuthor.username : null
-
         return (
             <div>
                 <div className="photo-show-env">
@@ -35,6 +36,7 @@ class PhotoShow extends React.Component {
                         <p className="photo-show-title" >{title}</p>
                         <p className="photo-show-description">{description}</p> 
                         {photoAuthor}
+                        {formattedCreatedAt}
                     </div>
                 </div>
             </div>
